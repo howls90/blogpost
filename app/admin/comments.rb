9013aa4106867575ee5c08c3dd9bf6958fdff11c
howls90 <mfloriach90@gmail.com>
2017-12-29 18:09:08 +0900

    activeadmin comments bug

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Category do
+ActiveAdmin.register Comment do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,6 +11,5 @@ ActiveAdmin.register Category do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-	permit_params :name
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Category.create(name: "ML")
+Category.create(name: "Android")
+Category.create(name: "Hacking")
+Category.create(name: "Games")
+Category.create(name: "FullStack")
